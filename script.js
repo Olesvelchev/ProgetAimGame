b16c21eb9af779ad70862c30c47a152c9a4f4f88
Олесь Вельчев <138475823+Olesvelchev@users.noreply.github.com>
2023-09-30 12:51:37 +0000

    fixed problems changed css

--- script.js
+++ script.js
@@ -1,229 +1,115 @@
 const startBtn = document.querySelector("#start"),
-    screens = document.querySelectorAll(".screen"),
-    timeList = document.querySelector("#time-list"),
-    difficultyList = document.querySelector("#difficulty-list"),
-    timeEl = document.querySelector("#time"),
-    board = document.querySelector("#board"),
-    hitsEL = document.querySelector("#hits"),
-    accuracyEl = document.querySelector("#accuracy"),
-    hitsOver = document.querySelector("#hits-over"),
-    accuracyOver = document.querySelector("#accuracy-over"),
-    hearts = document.querySelectorAll(".heart"),
-    restartBtns = document.querySelectorAll(".restart"),
-    fullScreenBtn = document.querySelector("#fullscreen"),
-    minimizeBtn = document.querySelector("#minimize");
+  screens = document.querySelectorAll(".screen"),
+  timeList = document.querySelector("#time-list"),
+  timeEl = document.querySelector("#time"),
+  board = document.querySelector("#board"),
+  colors = [
+    "#E8E46E",
+    "#01937C",
+    "7952B3",
+    "#B2B8A3",
+    "#F98404",
+    "#CF0000",
+    "#2940d3",
+    "#f4eee8"
+  ];
 
-let time = 0
-unlimited = false,
-    difficulty = 0,
-    playing = false,
-    hits = 0,
-    missed = 0,
-    accuracy = 0,
-    interval;
+  const restartBtn = document.createElement("button");
+restartBtn.innerHTML = "Restart";
+restartBtn.addEventListener("click", () => {
+  location.reload();
+});
+board.parentNode.insertBefore(restartBtn, board);
 
-startBtn.addEventListener("click", () => {
-    screens[0].classList.add("up");
+let time = 0,
+  score = 0;
+
+startBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  screens[0].classList.add("up");
 });
 
 timeList.addEventListener("click", (e) => {
-    if (e.target.classList.contains("time-btn")) {
-        time = parseInt(e.target.getAttribute("data-time"));
-        unlimited = e.target.getAttribute("data-unlimited");
-        screens[1].classList.add("up");
-    }
+  if (e.target.classList.contains("time-btn")) {
+    time = parseInt(e.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
 });
 
-difficultyList.addEventListener("click", (e) => {
-    if (e.target.classList.contains("difficulty-btn")) {
-        difficulty = parseInt(e.target.getAttribute("data-difficulty"))
-        screens[2].classList.add("up");
-        startGame();
-    }
-})
+board.addEventListener("click", (e) => {
+  if (e.target.classList.contains("circle")) {
+    score++;
+    e.target.remove();
+    createRandomCircle();
+  }
+});
 
 function startGame() {
-    palying = true;
-    interval = setInterval(decreaseTime, 1000);
-    createRandomCircle();
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+  setTime(time);
 }
 
 function decreaseTime() {
-
-    if (undefined) {
-        //if unlimited selected
-        setTime("∞")
-        return;
-    }
-    if (time === 0) {
-        //game over
-        finishGame();
-    }
+  if (time === 0) {
+    finishGame();
+  } else {
     let current = --time;
-    let miliseconds = time * 1000;
-
-    let minutes = Math.floor(miliseconds / (1000 * 60))
-    let seconds = Math.floor((miliseconds % (1000 * 60)) / 1000);
-
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-
-    setTime(`/${minutes}:${seconds}/`);
-
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTime(current);
+  }
 }
 
-function setTime(time) {
-    timeEl.innerHTML = time;
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+  timeEl.parentNode.classList.add("hide");
+  board.innerHTML = `<h1>Your Score: <span class = "primary">${score}</span></h1>`;
 }
 
 function createRandomCircle() {
-    if (!playing) {
-        return;
-    }
-    const circle = document.createElement("div");
-    const size = getRandomNumber(30, 100);
-    const colors = ["#03DAC6", "#FF0266", "#b3ff00", "#ccff00", "#9D00FF"];
-    const { width, height } = board.getBoundingCLientRect();
-    const x = getRandomNumber(0, width - size);
-    const y = getRandomNumber(0, height - size);
-    circle.classList.add("circle");
-    circle.style.width = `${size}px`;
-    circle.style.height = `${size}px`;
-    circle.style.top = `${y}px`;
-    circle.style.left = `${x}px`;
+  const circle = document.createElement("div"),
+    size = getRandomNumber(10, 60),
+    { width, height } = board.getBoundingClientRect(),
+    x = getRandomNumber(0, width - size),
+    y = getRandomNumber(0, height - size);
 
-    let color = Math.floor(Math.random() * 5);
-    circle.style.background = `${colors[color]}`;
-    board.append(circle);
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  setColor(circle);
 
-
-    //difficulty settings
-    if (difficulty === 1) {
-        circle.style.animationDuration = "2s";
-    } else if (difficulty === 2) {
-        circle.style.animationDuration = "1s";
-    } else {
-        circle.style.animationDuration = "3s";
-    }
-
-    circle.addEventListener("animationend", () => {
-        circle.remove();
-        createRandomCircle();
-        addMissed();
-        calculateAccuracy();
-    });
-
-    board.addEventListener("click", (e) => {
-        if (e.target.classList.contains("circle")) {
-            hits++;
-            e.target.remove();
-            createRandomCircle();
-        } else {
-            missed++;
-        }
-        hitsEL.innerHTML = hits;
-        calculateAccuracy();
-    })
-
-    function finishGame() {
-        finish = false;
-        clearInterval(interval);
-        board.innerHTML = "";
-        screens[3].classList.add("up");
-        hitsEL.innerHTML = 0;
-        timeEl.innerHTML = "00:00";
-        accuracy.innerHTML = "0%";
-        hitsOver.innerHTML = hits;
-        accuracyOver.innerHTML = `${accuracy}%`;
-    }
-
-    function addMissed() {
-
-        if (
-            hearts[0].classList.contains("dead") &&
-            hearts[1].classList.contains("dead") &&
-            hearts[2].classList.contains("dead") 
-            ) {
-                finishGame();
-
-        }
-        else {
-            missed++;
-
-            for (let i = 0; i < hearts.length; i++){
-                if(!hearts[i].classList.contains("dead")) {
-                    hearts[i].classList.add("dead");
-                    break;
-                }
-            }
-        }
-
+  board.append(circle);
 }
 
-    function calculateAccuracy() {
-        accuracy = (hits / (hits + missed)) * 100;
-        accuracy = accuracy.toFixed(2);
-        accuracyEl.innerHTML = `${accuracy}%`;
-    }
-
-    function getRandomNumber(min, max) {
-        return Math.random(Math.floor() * (man - min) + min);
-    }
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
 
-restartBtns.forEach((btn) =>{
-    btn.addEventListener("click", restartGame);
-});
-
-function restartGame() {
-    finishGame();
-    screens[1].classList.remove("up");
-    screens[2].classList.remove("up");
-    screens[3].classList.remove("up");
-    time = 0;
-    difficulty = 0;
-    hits = 0;
-    missed = 0;
-    accuracy = 0;
-    playinghits = false;
-    unlimited = false;
-    hearts.forEach((heart) => {
-        heart.classList.remove("dead");
-    });
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-fullScreenBtn.addEventListener("click" , fullScreen);
-
-let elem = document.documentElement;
-
-function fullScreen() {
-    if(elem.requestFullscreen) {
-        elem.requestFullscreen();
-    }else if(elem.mozRequestFullScreen) {
-        elem.mozRequestFullScreen();
-    }else if(elem.webkitRequestFullScreen) {
-        elem.webkitRequestFullScreen();
-    }else if (elem.msRequestFullScreen) {
-        elem.msRequestFullScreen();
-    }
-
-    fullScreenBtn.style.display - "none"
-    minimizeBtn.style.display - "block";
+function getRandomColor() {
+  const i = Math.round(Math.random() * colors.length);
+  return colors[i];
 }
 
-minimizeBtn.addEventListener("click" , minimize);
+function winTheGame() {
+  function kill() {
+    const circle = document.querySelector(".circle");
 
-function minimize() {
-    if (document.exitfullscreen) {
-        document.exitfullscreen();
-    }else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    }else if (document.webkitExitFullScreen) {
-        document.webkitExitFullScreen();
-    }else if (document.msExitFullScreen) {
-        document.msExitFullScreen();
-    }
-
-    minimizeBtn.style.display - "none"
-    fullScreenBtn.style.display - "block";
+    circle.click();
+  }
+  setInterval(kill, 75);
 }
+
